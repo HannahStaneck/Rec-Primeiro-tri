5ab03ead9a213a8b96b91d5c4c428ca68ac9b935
HannahStaneck <hannah.staneck15@gmail.com>
2021-08-21 02:26:08 -0300

    array
    array utilizado

---
+++ array.php
@@ -0,0 +1,55 @@
+<?php 
+
+
+
+$arrayBichos = array(
+	'1' => "<img src='img/1.png'>",
+	'2' => "<img src='img/2.png'>",
+	'3' => "<img src='img/3.png'>",
+	'4' => "<img src='img/4.png'>",
+	'5' => "<img src='img/5.png'>",
+	'6' => "<img src='img/6.png'>",
+	'7' => "<img src='img/7.png'>",
+	'8' => "<img src='img/8.png'>",
+	'9' => "<img src='img/9.png'>",
+	'10' => "<img src='img/10.png'>",
+	'11' => "<img src='img/11.png'>",
+	'12' => "<img src='img/12.png'>",
+	'13' => "<img src='img/13.png'>",
+	'14' => "<img src='img/14.png'>",
+	'15' => "<img src='img/15.png'>",
+	'16' => "<img src='img/16.png'>",
+	'17' => "<img src='img/17.png'>",
+	'18' => "<img src='img/18.png'>",
+	'19' => "<img src='img/19.png'>",
+	'20' => "<img src='img/20.png'>",
+	'21' => "<img src='img/21.png'>",
+	'22' => "<img src='img/22.png'>",
+	'23' => "<img src='img/23.png'>",
+	'24' => "<img src='img/24.png'>",
+	'25' => "<img src='img/25.png'>",
+);
+
+
+
+
+
+
+$vet = array('Avestruz' , 'Águia', 'Burro', 'Borboleta', 'Cachorro',
+    'Cabra', 'Carneiro', 'Camelo', 'Cobra', 'Coelho',
+    'Cavalo', 'Elefante', 'Galo', 'Gato', 'Jacaré',
+    'Leão', 'Macaco', 'Porco', 'Pavão', 'Peru',
+    'Touro', 'Tigre', 'Urso', 'Veado', 'Vaca');
+
+
+
+
+
+
+
+
+
+
+
+
+ ?>
